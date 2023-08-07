449ac5e59109a4a08a1daec0a93d06b5ce7af162
Wolf <augusto.woelfert@gmail.com>
2023-08-07 20:38:19 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <div>
+      </div>
+      <div>
+        <p>SECUENCIA BIEN HIJO DE PUTA</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
